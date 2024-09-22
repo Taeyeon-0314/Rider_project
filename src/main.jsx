f5046a30798d17ee40eaa45de438c531b689009d
Taeyeon <lty0314@gmail.com>
2024-09-22 17:59:37 +0900

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,15 @@
-// main.jsx or index.jsx
-import { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
-const loadKakaoMapScript = () => {
-  const script = document.createElement("script");
-  script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_KEY}&libraries=services,clusterer`;
-  script.async = true;
-  document.head.appendChild(script);
-};
-
-const AppWithKakaoMap = () => {
-  useEffect(() => {
-    loadKakaoMapScript();
-  }, []);
-
-  return <App />;
-};
-
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <AppWithKakaoMap />
+    <App />
+    {/* <ReactQueryDevtools /> */}
   </QueryClientProvider>
 );
